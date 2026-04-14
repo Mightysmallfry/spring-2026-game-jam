d7148e93b25cceb8eb6da24a1487500d882a4a1c
Mightysmallfry <90732393+Mightysmallfry@users.noreply.github.com>
2026-04-13 23:44:06 -0700

    Some base building stuff

---
+++ Buildings/base_building.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var sprite : Sprite2D
+
+var MAX_LEVEL : int = 9
+var INITIAL_LEVEL : int = 1
+var current_level : int = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
