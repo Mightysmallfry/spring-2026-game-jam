9df3655242a724b06d34b74c7a435f9ae41e500c
Mightysmallfry <90732393+Mightysmallfry@users.noreply.github.com>
2026-04-15 20:16:40 -0700

    Added fish variation

--- Fish/fish_resource.gd
+++ Fish/fish_resource.gd
@@ -5,7 +5,9 @@ class_name FishData
 @export var fishModifiers : Array[Enums.FishModifier]
 @export var fishRarity : Enums.FishRarity
 
+@export var texture : Texture2D
+
+# 10% variation
 @export var length : float
 @export var girth : float
 @export var weight : float
-@export var texture : Texture2D
