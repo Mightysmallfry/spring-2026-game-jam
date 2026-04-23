9ecb627f43d5735b0254e3bcc66554c3a9f7af90
Mightysmallfry <90732393+Mightysmallfry@users.noreply.github.com>
2026-04-22 19:10:37 -0700

    Added small blurs and expanded map

--- Menus/Pause/pause_menu.gd
+++ Menus/Pause/pause_menu.gd
@@ -1,26 +1,47 @@
 extends Control
 class_name PauseMenu
 
+@export var blurFrame : ColorRect
 
 func _ready() -> void:
 	Global.pause_menu = self
 	visible = false
 	
-	
 # Called when the node enters the scene tree for the first time.
 func _process(delta : float) -> void:
-	if (Input.is_action_just_pressed("pause")):
-		Global.pause()
-		
-	if (Global.game_paused):
-		visible = true
+	if (Input.is_action_just_pressed("pause") && !Global.game_paused):
 		Engine.time_scale = 0
-	else:
-		visible = false
+		Global.pause()
+		await blur_in()
+		visible = true
+		
+	elif (Input.is_action_just_pressed("pause") && Global.game_paused):
 		Engine.time_scale = 1
+		Global.pause()
+		await blur_out()
+		visible = false
 		
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 	
 func _on_resume_button_pressed() -> void:
 	Global.pause()
+
+func blur_in():
+	print("blurred")
+	var tween : Tween = create_tween()
+	tween.tween_property(
+		blurFrame.material,
+		"shader_parameter/lod",
+		3.0,
+		0.05
+	)
+	
+func blur_out():
+	var tween : Tween = create_tween()
+	tween.tween_property(
+		blurFrame.material,
+		"shader_parameter/lod",
+		0.0,
+		0.05
+	)
