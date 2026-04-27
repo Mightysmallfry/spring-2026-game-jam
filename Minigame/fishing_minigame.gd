ff3ed13bd79ab3d07d212a5741e4b2a734e69685
Mightysmallfry <90732393+Mightysmallfry@users.noreply.github.com>
2026-04-27 01:40:23 -0700

    Trying to improve the banners

--- Minigame/fishing_minigame.gd
+++ Minigame/fishing_minigame.gd
@@ -1,19 +1,26 @@
 extends Node2D
 
-#NOTE FOR J-BONE!!!!!
-#this emits a signal then hides itself, if you want to fish a new fish you need to call reset_for_new_fish(fish : FISHDATA)
-#I dont know how we were thinking of doing this but we could check if the fishing minigame exists and it
-#it doesent we create it and if it does we call reset_for_new_fish()
-#it should be ready for integration!
+
+signal fishing_finished(success : bool, fish : FishData)
+signal fish_caught(fish : FishData) #the pattern is for the movement type from the fish
+
 var MainGamePath : String = "res://World/Scenes/TestWorld.tscn"
+
+@export_group("Audio")
 @export var fishingGameMusic : AudioStream
 @export var successSong : AudioStream
 
+@export_group("Banners")
+@export var bannerDisplay : TextureRect
+@export var BANNER_HOOK : Texture
+@export var BANNER_CAUGHT : Texture
+@export var BANNER_MISS : Texture
 
-@onready var root_ui: Node2D = $UI_container
-@onready var catch_area: Sprite2D = $Fishing_bar_outside/Fishing_target
+@onready var root_ui : Node2D = $UI_container
+@onready var catch_area : Sprite2D = $Fishing_bar_outside/Fishing_target
+@onready var progressBar : TextureProgressBar = %TextureProgressBar
 
-@export var fish : FishData
+var currentFish : FishData
 var fish_in_bar = false
 
 const BASE_HOOK_WINDOW := 0.6
@@ -39,17 +46,15 @@ func _on_target_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "FishBar":
 		fish_in_bar = false
 
-signal fishing_finished(success : bool, fish : FishData)
-
-signal fish_caught(fish : FishData) #the pattern is for the movement type from the fish
 
 func _ready():
 	Global.audio_manager.play_music(fishingGameMusic)
 	
-	if not fish:
+	currentFish = FishBucket.get_random_fish()
+	if !currentFish:
 		push_warning("FishingMinigame requires passing in a fish to play")
 		return
-	$Difficulty_manager.dificulty = fish.fishRarity
+	$Difficulty_manager.dificulty = currentFish.fishRarity
 	
 func _physics_process(delta: float):
 	_elapsed += delta
@@ -63,20 +68,21 @@ func _physics_process(delta: float):
 		STATE.BITE:
 			_bite_timer -= delta * 1 #if we want to add rods we just change this to * rod.biteratetimer and pass in rod at the top
 			if _bite_timer <= 0.0:
-				$UI_container/Hook.visible = true
+				bannerDisplay.texture = BANNER_HOOK
+				bannerDisplay.visible = true
 				
 				_hook_fish()
 		STATE.HOOK:
 			_hook_window -= delta * 1
-			_flashbang($UI_container/Hook, _hook_window/4)
-			_engorge_ui($UI_container/Hook, 1.05,_hook_window/4)
-			_shake($UI_container/Hook,3.0,_hook_window/4)
+			_flashbang(bannerDisplay, _hook_window/4)
+			_engorge_ui(bannerDisplay, 1.05,_hook_window/4)
+			_shake(bannerDisplay,3.0,_hook_window/4)
 			#HOOK WINDOW, SHOW TEXT
 			if _hook_window <= 0.0:
-				$UI_container/Hook.visible = false
+				bannerDisplay.visible = false
 				_fail("Too slow")
 			elif Input.is_action_just_pressed("fish"):
-				$UI_container/Hook.visible = false
+				bannerDisplay.visible = false
 				_on_hook()
 		STATE.PLAY:
 			if fish_in_bar:
@@ -85,7 +91,7 @@ func _physics_process(delta: float):
 				_progress_val -= BASE_ESCAPE_DRAIN * delta
 			#upgrade_bar_color
 			_progress_val = clamp(_progress_val,0.0,100.0)
-			%TextureProgressBar.value = _progress_val
+			progressBar.value = _progress_val
 			if _progress_val >= 100:
 				_fishing_succeded = true
 				_state = STATE.END
@@ -98,30 +104,30 @@ func _physics_process(delta: float):
 				_end_screen_shown = true
 				_clean_fishing_minigame_for_display()
 				if _fishing_succeded:
-					var caught = $UI_container/CAUGHT
-					caught.visible = true
+					bannerDisplay.texture = BANNER_CAUGHT
+					bannerDisplay.visible = true
 					# These only trigger once now!
-					_shake(caught, 3.0, 0.5)
-					_flashbang(caught, 0.5)
-					_engorge_ui(caught, 1.1, 0.5)
+					_shake(bannerDisplay, 3.0, 0.5)
+					_flashbang(bannerDisplay, 0.5)
+					_engorge_ui(bannerDisplay, 1.1, 0.5)
 					await get_tree().create_timer(2.0).timeout
-					caught.visible = false
+					bannerDisplay.visible = false
 					Global.audio_manager.play_music(successSong)
 					await get_tree().create_timer(1.0).timeout
 					_display_fish() # Show the fish stats/sprite
 					_state = STATE.RESULTS
 					
 				else:
-					var to_Bad = $UI_container/To_Bad
-					to_Bad.visible = true
+					bannerDisplay.texture = BANNER_MISS
+					bannerDisplay.visible = true
 					await get_tree().create_timer(1.0).timeout
-					to_Bad.visible = false
+					bannerDisplay.visible = false
 					_fail("failed to catch")
 					Global.game_manager.change_2d_scene(MainGamePath, false, false)
 
 		STATE.RESULTS: #Show STATS!
 			if Input.is_action_just_pressed("fish"):
-				fishing_finished.emit(_fishing_succeded,fish)
+				fishing_finished.emit(_fishing_succeded, currentFish)
 				#The thing restarts so If you want to reset the game pause it as soon as you get the signal
 				_deactivate_game()
 				Global.game_manager.change_2d_scene(MainGamePath, false, false)
@@ -138,11 +144,8 @@ func _clean_tweens():
 	
 func _set_visuals_on_restart():
 	#Hide End Game UI
-	$UI_container/Panel.visible = false
-	$UI_container/Hook.visible = false
+	bannerDisplay.visible = false
 	$UI_container/ChoppingBlock.visible = false
-	$UI_container/CAUGHT.visible = false
-	$UI_container/To_Bad.visible = false
 	
 	#ensuring the root is visible just in case
 	root_ui.visible = true
@@ -157,8 +160,8 @@ func _set_visuals_on_restart():
 	
 	#Show Gameplay UI
 	$Fishing_bar_outside.visible = true
-	%TextureProgressBar.value = 0.0
-	%TextureProgressBar.visible = true
+	progressBar.value = 0.0
+	progressBar.visible = true
 	
 func _hook_fish():
 	_state = STATE.HOOK
@@ -170,10 +173,10 @@ func _hook_fish():
 func take_damage():
 	_progress_val -= 15
 	_progress_val = clamp(_progress_val,0,100)
-	%TextureProgressBar.value = _progress_val
+	progressBar.value = _progress_val
 
 func _on_hook():
-	fish_caught.emit(fish)
+	fish_caught.emit(currentFish)
 	_progress_val = 15.0 #so you dont insta fail
 	_time_stop(0.06)
 	_shake(root_ui,6.0,0.18)
@@ -185,8 +188,8 @@ func _fail(_why: String):
 	#play hook fail sfx
 	_shake(root_ui,5.0,0.20)
 	$Fishing_bar_outside.visible = false
-	$UI_container/TextureProgressBar.visible = false
-	fishing_finished.emit(_fishing_succeded,fish)
+	progressBar.visible = false
+	fishing_finished.emit(_fishing_succeded, currentFish)
 	#the game should be paused as soon as you get the signl
 	#_distroy_game()
 	#await get_tree().create_timer(0.2).timeout
@@ -204,9 +207,9 @@ func reset_for_new_fish(new_fish_data : FishData):
 	_bite_timer = 2.0
 	_progress_val = 0.0
 	_end_screen_shown = false
-	fish = new_fish_data
+	currentFish = new_fish_data
 	#Update Difficulty for the new fish!
-	$Difficulty_manager.dificulty = fish.fishRarity
+	$Difficulty_manager.dificulty = currentFish.fishRarity
 	#reset visuals to default
 	_set_visuals_on_restart()
 	#wake
@@ -242,7 +245,7 @@ func _clean_fishing_minigame_for_display():
 	for enemy in $Enemies.get_children():
 		enemy.queue_free()
 	$Fishing_bar_outside.visible = false
-	$UI_container/TextureProgressBar.visible = false
+	progressBar.visible = false
 	
 func _display_fish():
 	var fishSprite = $UI_container/PotentialFish
@@ -250,22 +253,22 @@ func _display_fish():
 	var vbox = $UI_container/Panel/VBoxContainer
 	var panel = $UI_container/Panel
 	panel.visible = true
-	vbox.get_node("Name").text = "Name: " + fish.fishName
-	var rarity_name = Enums.FishRarity.keys()[fish.fishRarity]
+	vbox.get_node("Name").text = "Name: " + currentFish.fishName
+	var rarity_name = Enums.FishRarity.keys()[currentFish.fishRarity]
 	vbox.get_node("Rarity").text = "Rarity: " + rarity_name
 	
-	var display_weight = snapped(fish.weight + randf_range(-1.5, 1.5), 0.01)
+	var display_weight = snapped(currentFish.weight + randf_range(-1.5, 1.5), 0.01)
 	vbox.get_node("Weight").text = "Weight: " + str(display_weight)
 	
-	var display_girth = snapped(fish.girth + randf_range(-1.5, 1.5), 0.01)
+	var display_girth = snapped(currentFish.girth + randf_range(-1.5, 1.5), 0.01)
 	vbox.get_node("Girth").text = "Girth: " + str(display_girth)
 	
-	var display_length = snapped(fish.length + randf_range(-1.5, 1.5), 0.01)
+	var display_length = snapped(currentFish.length + randf_range(-1.5, 1.5), 0.01)
 	vbox.get_node("Length").text = "Length: " + str(display_length)
 	
 	_flashbang(vbox,0.5)
 	
-	fishSprite.texture = fish.texture
+	fishSprite.texture = currentFish.texture
 	choppingblock.visible = true
 	
 	fishSprite.visible = true
